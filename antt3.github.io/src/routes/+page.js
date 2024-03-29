1795e974072e30f6d9580061a13d6b6e70ff2bde
Anthony T <anthonyt3@sbcglobal.net>
2024-03-28 21:01:09 -0700

    Updated To New Syntax & Fixed Undefined Data Error

---
+++ antt3.github.io/src/routes/+page.js
@@ -0,0 +1,28 @@
+import { gql, GraphQLClient } from 'graphql-request'
+
+export const load = async () => {
+    const client = new GraphQLClient(
+        import.meta.env.VITE_GRAPHQL_API
+    )
+
+    const query = gql`
+        query GetProjects {
+            projects {
+                name
+                slug
+                description
+                demo
+                sourceCode
+                image {
+                    url
+                }
+            }
+        }
+    `
+
+    const { projects } = await client.request(query)
+
+    return {
+        projects
+    }
+}
