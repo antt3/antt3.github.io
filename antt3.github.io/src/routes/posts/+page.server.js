c8a65d763329bba8559193e0da780f529a76f89b
Anthony T <anthonyt3@sbcglobal.net>
2024-03-30 10:09:57 -0700

    Fixed Posts Page Import Error Typo

--- antt3.github.io/src/routes/posts/+page.server.js
+++ antt3.github.io/src/routes/posts/+page.server.js
@@ -1,5 +1,5 @@
 import { client } from '$lib/graphql/client'
-import { postsQuery } from '$/lib/graphql/queries'
+import { postsQuery } from '$lib/graphql/queries'
 
 export const load = async () => {
     const { posts } = await client.request(postsQuery)
