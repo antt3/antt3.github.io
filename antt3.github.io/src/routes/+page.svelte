e264f1d188f45a9a2c8f9586cb17a2871577eba3
Anthony T <anthonyt3@sbcglobal.net>
2024-03-29 22:03:12 -0700

    Added Styling To Author Component

--- antt3.github.io/src/routes/+page.svelte
+++ antt3.github.io/src/routes/+page.svelte
@@ -1,25 +1,23 @@
 <script>  
     import ProjectCard from '$lib/components/project-card.svelte'
+    import AuthorCard from '../lib/components/author-card.svelte';
 
     export let data
     let { projects, authors } = data
     $: ({ projects, authors } = data)
 </script>
 
-<h1>Projects</h1>
+<h1 class="font-bold text-center mb-20 text-5xl">
+    Welcome To My Portfolio!
+</h1>
 
 {#each authors as { name, intro, picture: { url } }}
-    <div>
-        <div>
-            <h2>{name}</h2>
-            <p>{intro}</p>
-        </div>
-
-        <img src={url} alt={name} />
-    </div>   
+    <AuthorCard {name} {intro} {url} />
 {/each}
 
-<div>
+<div
+    class="grid gap-10 md:grid-cols-4 md:px-10 lg:grid-cols-6 lg:-mx-52"
+>
     {#each projects as { name, slug, description, image }}
         <ProjectCard {name} {description} url={image[0].url} {slug} />
     {/each}
