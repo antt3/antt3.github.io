47c0c07d560bcde459f4dc17f17e6eba684e068f
Anthony T <anthonyt3@sbcglobal.net>
2024-03-31 15:53:28 -0700

    Added Basic Footer

--- antt3.github.io/src/routes/+layout.svelte
+++ antt3.github.io/src/routes/+layout.svelte
@@ -1,7 +1,9 @@
 <script>
+	import Footer from '$lib/components/footer.svelte';
 	import '../app.pcss';
 </script>
 
 <main class="container max-w-3xl mx-auto px-4 mb-20">
 	<slot />
 </main>
+<Footer />
