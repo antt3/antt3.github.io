3c864279ad8540186e7dd99b8efdad8cacdf4dfc
Anthony T <anthonyt3@sbcglobal.net>
2024-03-31 16:56:19 -0700

    Added Navbar & Fixed tabindex Warning By Adding Roles

--- antt3.github.io/src/routes/+layout.svelte
+++ antt3.github.io/src/routes/+layout.svelte
@@ -1,8 +1,10 @@
 <script>
+	import Navbar from '$lib/components/navbar.svelte';
 	import Footer from '$lib/components/footer.svelte';
 	import '../app.pcss';
 </script>
 
+<Navbar />
 <main class="container max-w-3xl mx-auto px-4 mb-20">
 	<slot />
 </main>
